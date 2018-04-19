4bfbb4d73122d0b8dffa2846c7e1e8189f8afffd
studentuser <ml15aad@cl-2e102.herts.ac.uk>
2018-04-19 14:16:45 +0100

    Drag

---
+++ FlyingGameTry2/PlaneMovement.swift
@@ -0,0 +1,13 @@
+//
+//  PlaneMovement.swift
+//  FlyingGameTry2
+//
+//  Created by ml15aad on 19/04/2018.
+//  Copyright © 2018 ml15aad. All rights reserved.
+//
+
+import UIKit
+
+class PlaneMovement: NSObject {
+
+}
