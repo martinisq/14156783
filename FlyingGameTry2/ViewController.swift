0fadf03abe853a6c78cf4bce84f8a919c49f2dc5
Martynas L <martynas@martynass-mac.local>
2018-04-19 12:53:49 -0700

    WIP Collision

--- FlyingGameTry2/ViewController.swift
+++ FlyingGameTry2/ViewController.swift
@@ -15,26 +15,23 @@ class ViewController: UIViewController, planeDelegate {
     
     var dynamicAnimator: UIDynamicAnimator!
     var dynamicItemBehavior: UIDynamicItemBehavior!
+    var objectGravity:UIGravityBehavior!
+    var objectCollision:UICollisionBehavior!
     func changePlaneMovement() {
     }
-    
-    
     
     
     @IBOutlet weak var background1: UIImageView!
     @IBOutlet weak var background2: UIImageView!
     @IBOutlet weak var planeImage: PlaneMovement!
+    var enemyPlane1 = UIImageView(image:nil)
+    var enemyPlane2 = UIImageView(image:nil)
+    var enemyPlane3 = UIImageView(image:nil)
+    var timer = Timer()
     
     
     
-    
-    
-    
-    
-    
-    
-    
-    
+  
     
     
     
@@ -42,7 +39,42 @@ class ViewController: UIViewController, planeDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         backgroundLoop()
+        enemyPlanes()
+        enemyTimer()
     }
+    
+    func enemyTimer(){
+        timer = Timer.scheduledTimer(timeInterval: 4, target: self, selector:#selector(ViewController.enemyPlanes), userInfo: nil, repeats: true)
+    }
+    
+    
+    func enemyPlanes(){
+        
+        enemyPlane1.image=UIImage(named: "eplane1")
+        enemyPlane1.frame=CGRect(x:Int(arc4random_uniform(301)),y:-500,width:80,height:95)
+        self.view.addSubview(enemyPlane1)
+        
+        enemyPlane2.image=UIImage(named:"eplane2")
+        enemyPlane2.frame=CGRect(x:Int(arc4random_uniform(301)),y:-900,width:80,height:95)
+        self.view.addSubview(enemyPlane2)
+        
+        enemyPlane3.image=UIImage(named: "eplane3")
+        enemyPlane3.frame=CGRect(x:Int(arc4random_uniform(301)),y:-100,width:80,height:95)
+        self.view.addSubview(enemyPlane3)
+        
+        
+        dynamicAnimator = UIDynamicAnimator(referenceView: self.view)
+        objectGravity = UIGravityBehavior(items:[enemyPlane1,enemyPlane2,enemyPlane3])
+        objectGravity.magnitude = 0.3
+        dynamicAnimator.addBehavior(objectGravity)
+     }
+    
+    func planeHitbox(){
+        let hitbox =
+        
+    }
+    
+    
     
     
    
