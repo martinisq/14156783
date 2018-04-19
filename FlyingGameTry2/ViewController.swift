b17afe5a04ad2f2b63e2471f5c7b21a4eaae30c5
studentuser <ml15aad@cl-2e102.herts.ac.uk>
2018-04-19 13:41:46 +0100

    Initial Commit

---
+++ FlyingGameTry2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlyingGameTry2
+//
+//  Created by ml15aad on 19/04/2018.
+//  Copyright © 2018 ml15aad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
